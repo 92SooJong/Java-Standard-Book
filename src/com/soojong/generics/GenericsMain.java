92109688467f7058abadb717838e7c4465e01fdf
하수종 <hsz3855@gmail.com>
2022-10-03 02:00:47 +0900

    Printer with Generic

--- src/com/soojong/generics/GenericsMain.java
+++ src/com/soojong/generics/GenericsMain.java
@@ -1,10 +1,17 @@
 package com.soojong.generics;
 
+import java.util.ArrayList;
+
 public class GenericsMain {
 
     public static void main(String[] args) {
-        IntegerPrinter integerPrinter = new IntegerPrinter(24);
-        integerPrinter.print();
+//        IntegerPrinter integerPrinter = new IntegerPrinter(24);
+//        integerPrinter.print();
+        Printer<Integer> printer = new Printer<>(23);
+        printer.print();
+
+        Printer<Double> printerForString = new Printer<>(33.5);
+        printer.print();
 
     }
 }
