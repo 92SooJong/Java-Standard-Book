21fc5f54afd48d2130df4902d802c661c7b2df60
하수종 <hsz3855@gmail.com>
2022-10-15 15:38:08 +0900

    create Math Util

--- src/com/soojong/util/math/MathMain.java
+++ src/com/soojong/util/math/MathMain.java
@@ -4,40 +4,9 @@ public class MathMain {
 
     public static void main(String[] args) {
 
-        int number = 96;
-        int lastDigitZero = (number / 10) * 10;
-        int lastDigit = number%lastDigitZero;
+        System.out.println(MathUtil.getMultipleNumber(163,6 , true));
 
-        System.out.println("original : "+number);
-        System.out.println("lastDigitZero : "+lastDigitZero);
-        System.out.println("lastDigit : "+lastDigit);
-        int result1 = lastDigitZero;
-        if(lastDigit > 4) {
-            result1 = lastDigitZero + 5;
-        }
-        System.out.println(result1);
-        System.out.println("======================");
-
-        int number2 = 255;
-        int lastDigitZero2 = (number2 / 10) * 10;
-        int lastDigit2 = number2%lastDigitZero2;
-        int result2 = 0;
-        System.out.println("original : "+number2);
-        System.out.println("lastDigitZero : "+lastDigitZero2);
-        System.out.println("lastDigit : "+lastDigit2);
-
-        if(lastDigit2 > 4) {
-            result2 = lastDigitZero2+9;
-        } else {
-            result2 = lastDigitZero2+4;
-        }
-        System.out.println(result2);
-
-
-        System.out.println("======================");
-        for(int s = result1; s<=result2; s=s+5 ) {
-            System.out.printf("from : %d -> to : %d\n", s , s+4);
-        }
+        System.out.println(MathUtil.getMultipleNumber(163,6 , false));
 
     }
 
