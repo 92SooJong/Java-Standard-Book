fcbb1cb3676e4e9f0617fc9a95c8e00fbfa24491
하수종 <hsz3855@gmail.com>
2022-10-26 20:38:22 +0900

    add Function chain

--- src/com/soojong/functional/_Function.java
+++ src/com/soojong/functional/_Function.java
@@ -8,13 +8,24 @@ public class _Function {
         int increment = increment(0);
         System.out.println(increment);
 
-        Integer apply = incrementByOneFunction.apply(1);
-        System.out.println(apply);
+        Integer incrementOneByFunction = incrementByOneFunction.apply(1);
+        System.out.println(incrementOneByFunction);
 
+        Integer multiply = multipleBy10Function.apply(increment);
+        System.out.println(multiply);
+
+
+        // chaining
+        Function<Integer, Integer> integerIntegerFunction = incrementByOneFunction.andThen(multipleBy10Function);
+        Integer apply1 = integerIntegerFunction.apply(4);
+        System.out.println(apply1);
 
     }
 
     static Function<Integer, Integer> incrementByOneFunction = number -> number + 1;
+
+    static Function<Integer, Integer> multipleBy10Function = number -> number * 10;
+
 
     static int increment(int number) {
         return number + 1;
