115167d385e70b58d2393e34589e2553afb4cfed
하수종 <hsz3855@gmail.com>
2022-10-26 20:46:48 +0900

    add BiFunction

--- src/com/soojong/functional/_Function.java
+++ src/com/soojong/functional/_Function.java
@@ -1,11 +1,12 @@
 package com.soojong.functional;
 
+import java.util.function.BiFunction;
 import java.util.function.Function;
 
 public class _Function {
 
     public static void main(String[] args) {
-        int increment = increment(0);
+        int increment = incrementByOne(0);
         System.out.println(increment);
 
         Integer incrementOneByFunction = incrementByOneFunction.apply(1);
@@ -14,11 +15,15 @@ public class _Function {
         Integer multiply = multipleBy10Function.apply(increment);
         System.out.println(multiply);
 
-
-        // chaining
+        // Function Chain
         Function<Integer, Integer> integerIntegerFunction = incrementByOneFunction.andThen(multipleBy10Function);
         Integer apply1 = integerIntegerFunction.apply(4);
         System.out.println(apply1);
+
+        // BiFunction takes 2 argument and produces 1 result
+        int incrementByOneAndMultiply = incrementByOneAndMultiplyBiFunction.apply(4, 100);
+        System.out.println(incrementByOneAndMultiply);
+
 
     }
 
@@ -27,9 +32,14 @@ public class _Function {
     static Function<Integer, Integer> multipleBy10Function = number -> number * 10;
 
 
-    static int increment(int number) {
+    static int incrementByOne(int number) {
         return number + 1;
     }
 
+    static BiFunction<Integer, Integer, Integer> incrementByOneAndMultiplyBiFunction = (number, numToMultiplyBy) -> (number + 1) * numToMultiplyBy;
+
+    static int incrementByOneAndMultiply(int number, int numToMultiplyBy) {
+        return (number + 1) * numToMultiplyBy;
+    }
 
 }
