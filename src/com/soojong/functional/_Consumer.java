d345c8c88fed094090be85c321ad429014e8c9d5
하수종 <hsz3855@gmail.com>
2022-10-26 23:59:13 +0900

    add Consumer, BiConsumer sample

---
+++ src/com/soojong/functional/_Consumer.java
@@ -0,0 +1,46 @@
+package com.soojong.functional;
+
+import java.util.function.BiConsumer;
+import java.util.function.Consumer;
+
+public class _Consumer {
+
+    public static void main(String[] args) {
+        greetCustomer(new Customer("Maria", "99999"));
+
+        // Consumer Functional interface
+        greetCustomerConsumer.accept(new Customer("Maria", "99999"));
+        greetCustomerConsumerV2.accept(new Customer("Maria", "99999"), false);
+    }
+
+    static BiConsumer<Customer, Boolean> greetCustomerConsumerV2 = (customer, showPhoneNumber) ->
+        System.out.println("hello"
+            + customer.customerName
+            + ", thanks for registering phone number "
+            + (showPhoneNumber ? customer.customerPhoneNumber : "******"));
+
+    static Consumer<Customer> greetCustomerConsumer = customer ->
+        System.out.println("hello"
+            + customer.customerName
+            + ", thanks for registering phone number "
+            + customer.customerPhoneNumber);
+
+    static void greetCustomer(Customer customer) {
+        System.out.println("hello"
+            + customer.customerName
+            + ", thanks for registering phone number "
+            + customer.customerPhoneNumber);
+    }
+
+    static class Customer {
+        private final String customerName;
+        private final String customerPhoneNumber;
+
+        public Customer(String customerName, String customerPhoneNumber) {
+            this.customerName = customerName;
+            this.customerPhoneNumber = customerPhoneNumber;
+        }
+    }
+
+
+}
