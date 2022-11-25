231ed0be56bb74dea30c4e06a04599f29929304c
하수종 <hsz3855@gmail.com>
2022-11-25 21:47:19 +0900

    Add Map Rehashing

---
+++ src/com/soojong/datastructure/rehash/RehashingMain.java
@@ -0,0 +1,33 @@
+package com.soojong.datastructure.rehash;
+
+import java.util.Map;
+
+public class RehashingMain {
+
+    public static void main(String[] args) {
+        // Creating the Map
+        RehashingSampleMap<Integer, String> map = new RehashingSampleMap<>();
+
+        // Inserting elements
+        map.insert(1, "Geeks");
+        map.printMap();
+
+        map.insert(2, "forGeeks");
+        map.printMap();
+
+        map.insert(3, "A");
+        map.printMap();
+
+        map.insert(4, "Computer");
+        map.printMap();
+
+        map.insert(5, "Portal");
+        map.printMap();
+
+        //Get element from Map
+        int key = 4;
+        String value = map.getValue(key);
+        System.out.println("Value at key "+ key +" is: "+ value);
+    }
+
+}
