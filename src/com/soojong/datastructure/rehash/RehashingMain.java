633c0f7e117e4dd83f74c59f8f1f0359fe1b5cdd
하수종 <hsz3855@gmail.com>
2022-11-29 16:25:40 +0900

    change key type to String

--- src/com/soojong/datastructure/rehash/RehashingMain.java
+++ src/com/soojong/datastructure/rehash/RehashingMain.java
@@ -1,33 +1,35 @@
 package com.soojong.datastructure.rehash;
 
+import java.util.HashMap;
 import java.util.Map;
 
 public class RehashingMain {
 
     public static void main(String[] args) {
         // Creating the Map
-        RehashingSampleMap<Integer, String> map = new RehashingSampleMap<>();
+        RehashingSampleMap<String, String> map = new RehashingSampleMap<>();
 
         // Inserting elements
-        map.insert(1, "Geeks");
+        map.insert("1", "Geeks");
         map.printMap();
 
-        map.insert(2, "forGeeks");
+        map.insert("2", "forGeeks");
         map.printMap();
 
-        map.insert(3, "A");
+        map.insert("3", "A");
         map.printMap();
 
-        map.insert(4, "Computer");
+        map.insert("4", "Computer");
         map.printMap();
 
-        map.insert(5, "Portal");
+        map.insert("5", "Portal");
         map.printMap();
 
         //Get element from Map
-        int key = 4;
+        String key = "4";
         String value = map.getValue(key);
         System.out.println("Value at key "+ key +" is: "+ value);
+
     }
 
 }
