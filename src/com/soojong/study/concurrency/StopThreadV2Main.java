84cb547610b0422c2128e7b8b86bd9c3af61ebe1
Joey <hsz3855@gmail.com>
2023-08-07 00:44:00 +0900

    volatile, synchronized 코드 추가

--- src/com/soojong/study/concurrency/StopThreadV2Main.java
+++ src/com/soojong/study/concurrency/StopThreadV2Main.java
@@ -5,23 +5,33 @@ import java.util.concurrent.TimeUnit;
 /**
  * 이펙티브 자바 아이템 78의 예제 코드입니다. (공유 중인 가변 데이터는 동기화해 사용하라)
  */
-public class StopThreadMain {
+public class StopThreadV2Main {
 
     private static boolean stopRequested;
+
+    private static synchronized void requestStop() {
+        stopRequested = true;
+    }
+
+    private static synchronized boolean stopRequested() {
+        return stopRequested;
+    }
+
 
     public static void main(String[] args) throws InterruptedException {
 
         Thread backgroundThread = new Thread(() -> {
            int i = 0;
-           while (!stopRequested) { // 메인쓰레드가 수정한 값을 백그래안두 쓰레드가 언제 보게될지 알수 없음.
+           while (!stopRequested()) {
                i++;
            }
-
+           
         });
         backgroundThread.start(); // 백그라운드 쓰레드 시작
 
         TimeUnit.SECONDS.sleep(1); // 메인 쓰레드 1초 sleep
-        stopRequested = true; // 메인 쓰레드가 변경
+        requestStop();
+
     }
 
 }
--- src/com/soojong/study/concurrency/StopThreadV3Main.java
+++ src/com/soojong/study/concurrency/StopThreadV3Main.java
@@ -5,17 +5,17 @@ import java.util.concurrent.TimeUnit;
 /**
  * 이펙티브 자바 아이템 78의 예제 코드입니다. (공유 중인 가변 데이터는 동기화해 사용하라)
  */
-public class StopThreadMain {
+public class StopThreadV3Main {
 
-    private static boolean stopRequested;
+    private static volatile boolean stopRequested;
 
     public static void main(String[] args) throws InterruptedException {
 
         Thread backgroundThread = new Thread(() -> {
-           int i = 0;
-           while (!stopRequested) { // 메인쓰레드가 수정한 값을 백그래안두 쓰레드가 언제 보게될지 알수 없음.
-               i++;
-           }
+            int i = 0;
+            while (!stopRequested) { // 메인쓰레드가 수정한 값을 백그래안두 쓰레드가 언제 보게될지 알수 없음.
+                i++;
+            }
 
         });
         backgroundThread.start(); // 백그라운드 쓰레드 시작
