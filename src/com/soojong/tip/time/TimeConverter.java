9b145d4152032c8ee91167bb63d792d7b53df7d8
Joey <hsz3855@gmail.com>
2023-08-28 23:08:47 +0900

    add : 각 국가의 저녁 6시를 UTC 시간으로 변환하는 로직을 추가한다

---
+++ src/com/soojong/tip/time/TimeConverter.java
@@ -0,0 +1,31 @@
+package com.soojong.tip.time;
+
+import java.time.LocalDateTime;
+import java.time.ZoneId;
+import java.time.ZoneOffset;
+import java.time.ZonedDateTime;
+
+public class TimeConverter {
+
+
+    public static LocalDateTime get6clockOfUTC(CountryEnum countryEnum) {
+
+
+        // 입력으로 받은 국가의 시간을 18시로 세팅한다
+        ZoneId zoneId = ZoneId.of(countryEnum.getTimeZone());
+        ZonedDateTime zonedDateTime = ZonedDateTime
+            .of(LocalDateTime.now(), zoneId)
+            .withHour(18)
+            .withMinute(0)
+            .withSecond(0);
+
+        // 해당 시간을 UTC로 환산한다.
+        ZoneId utcZoneId = ZoneId.of(ZoneOffset.UTC.getId());
+        LocalDateTime utcLocalDateTime = zonedDateTime.withZoneSameInstant(utcZoneId)
+            .toLocalDateTime();
+
+        return utcLocalDateTime;
+    }
+
+
+}
--- src/com/soojong/tip/time/TimeMain.java
+++ src/com/soojong/tip/time/TimeMain.java
@@ -2,16 +2,21 @@ package com.soojong.tip.time;
 
 import java.time.LocalDateTime;
 import java.time.ZoneId;
+import java.time.ZoneOffset;
+import java.time.ZonedDateTime;
 
 public class TimeMain {
 
     public static void main(String[] args) {
 
-        printAlarmStatus("ko");
-        printAlarmStatus("jp");
-        printAlarmStatus("sg");
-        printAlarmStatus("us");
+//        printAlarmStatus("ko");
+//        printAlarmStatus("jp");
+//        printAlarmStatus("sg");
+//        printAlarmStatus("us");
 
+        System.out.println(TimeConverter.get6clockOfUTC(CountryEnum.JAPAN));
+        System.out.println(TimeConverter.get6clockOfUTC(CountryEnum.KOREA));
+        System.out.println(TimeConverter.get6clockOfUTC(CountryEnum.UNITED_STATE));
     }
 
     public static void printAlarmStatus(String countryCode) {
@@ -19,7 +24,9 @@ public class TimeMain {
         CountryEnum countryEnum = CountryEnum.findByCode(countryCode);
 
         ZoneId zoneId = ZoneId.of(countryEnum.getTimeZone());
-        LocalDateTime localDateTime = LocalDateTime.now(zoneId);
+        LocalDateTime localDateTime = LocalDateTime.now(zoneId).withHour(18).withMinute(0);
+
+
 
         System.out.printf("Not allowed time range for alarm in %s is %s\n", zoneId.getId(), countryEnum.getNotAllowedAlramTimePeriod().toString());
         System.out.printf("Current time in %s is %s\n", zoneId.getId(), localDateTime);
